cf76c133d73acd53ac93fb512a92baafb4c8c630
Julian Nonino <noninojulian@gmail.com>
2023-09-08 12:37:17 +0100

    Add pipelines

---
+++ .github/workflows/scripts/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Environment variables
+if [[ -z "${ENV}" ]]; then
+    echo "ENV should be set (development|production)"
+    exit 1
+else
+    echo "Building website for $ENV"
+fi
+
+# For maximum backward compatibility with Hugo modules
+HUGO_ENVIRONMENT=$ENV
+HUGO_ENV=$ENV
+
+# Build site
+hugo --gc --minify --noBuildLock \
+  --printPathWarnings --printI18nWarnings \
+  --printMemoryUsage --printUnusedTemplates \
+  --templateMetrics --templateMetricsHints \
+  --environment $ENV
+
+# Add CNAME config if it is production
+if [ $ENV == "production" ]; then
+  echo 'learn-software.com' > public/CNAME
+fi
